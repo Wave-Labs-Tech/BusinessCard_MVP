a354b4b878e5f2cbd98724d2fcf1e321e7640874
ariel robotti <arielrobotti@gmail.com>
2024-09-03 23:58:36 -0300

    Mapping address  => CompanyID; CompanyID => Company

--- script/BusinnesCard.sol
+++ script/BusinnesCard.sol
@@ -3,66 +3,73 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./Structs.sol";
 
 contract BusinnesCard is ERC721 {
+    address constant ZERO_ADDRESS = address(0);
+    address public owner;
 
-    constructor() ERC721("BusinessCard", "BC") {}
-
-    // mapping(uint256 => )
-
-    //Struct para enviar a la funcion mintear BC
-    mapping(uint256 => Company) companies;
-
-    struct Company {
-        uint256 companyID;
-        string companyName;
-        string companyAddress;
-        string companyWebsite;
-        string companyEmail;
-        uint256 companyPhone;
-        string companyIndustry; //cambiar por un enum con enumeraciones predefinidas
-        string companyFoundedYear;
-        string companyCEO;
-        uint16 companyEmployees;
-        string companyDescription;
+    constructor() ERC721("BusinessCard", "BC") {
+        owner = msg.sender;
+    }
+    //////////// Modificadores ///////////////
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can execute this function");
+        _;
     }
 
-    struct CardDataInit { 
-        string name;
-        string email;
-        uint256 companyID;
-        string cargo;  
-        uint256 phone;
-        string[] URLs; //Si no se proporcionan URL llega una lista vacia
-        // string photo; //Token URI ipfs
+    modifier onlyCompanies() {
+        require(companiesID[msg.sender].exists);
+        _;
     }
-    // Struct de retorno para funciones de acceso a datos publicos
-    struct PublicInfoCard {
-        uint256 id;
-        string name;
-        string photo;
-        string[] URLs;
-        uint256 score; 
-        uint256 numberOfContacts;
+
+    //////////////////////////////////////////////
+
+    uint16 nextCompanyID = 1;
+    uint256 nextCardID = 1;
+    uint256 feeCreateCompany;
+
+    function setFeeCreateCompany(uint256 _fee) public onlyOwner{
+        feeCreateCompany = _fee;
     }
-    // Struct de retorno para funciones de acceso a datos publicos
-    struct PrivateInfoCard {
-        uint256 phone;
-        string email;
+
+    mapping(address => Structs.FullInfoCard) cards;
+    // Evaluar la asignación de un id de empresa como valor y la inclusion de un map (IDEmpresa => Company) para evitar que la address
+    // de las empresas queden expuestas en las business card
+    mapping(address => Structs.CompID) companiesID;
+    mapping(uint16 => Structs.Company) companies; 
+
+    event CompanyCreated(address indexed companyAddress, uint16 companyID);
+    
+    // Esta funcion crea un perfil de empresa que queda vinculado uno a uno a la address del sender
+    function createCompany(Structs.CompanyInit memory initValues) public payable {
+        require(!companiesID[msg.sender].exists, "Company already exists");
+        require(msg.value >= feeCreateCompany, "Insufficient payment");
+        /// Devolución de excedente (revisar)
+        if (msg.value > feeCreateCompany) {
+            payable(msg.sender).transfer(msg.value - feeCreateCompany);
+        }
+        ///// Ver que hacer con los fondos ///////////////
+        Structs.Company memory newCompany = Structs.Company({
+            initValues: initValues,
+            companyEmployees: 0,
+            scoring: 0,
+            verified: false
+        });
+        companiesID[msg.sender] = Structs.CompID({id: nextCompanyID, exists: true}); 
+        companies[nextCompanyID] = newCompany;
+        emit CompanyCreated(msg.sender, nextCompanyID);
+        nextCompanyID ++;
     }
-    struct FullInfoCard {
-        PublicInfoCard publicInfo;
-        PrivateInfoCard privateInfo;
+
+    function createCardFor(Structs.CardDataInit memory _initValues, address _for) public onlyCompanies{    
+
     }
-    enum ConectionType {
-        Initial,
-        Professional,
-        Personal
+
+    function createMyCard(Structs.CardDataInit memory initValues) public {
+        require(!cards[msg.sender].exists, "There is already a card associated with your address");
+
     }
-    struct Contact {
-        uint256 timestamp;
-        uint256 counterConnections;
-        ConectionType kind;
-    }
+
 
 }
