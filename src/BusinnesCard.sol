2f442568bc3018115410828d841eb36b7c9fe977
ariel robotti <arielrobotti@gmail.com>
2024-09-09 11:46:49 -0300

    NatSpec comments

--- src/BusinnesCard.sol
+++ src/BusinnesCard.sol
@@ -3,31 +3,60 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import { CardDataInit } from "./models/CardDataInit.sol";
 import { Card } from "./models/Card.sol";
+import { CardDataInit } from "./models/CardDataInit.sol";
 import { Company } from "./models/Company.sol";
 import { CompanyInit } from "./models/CompanyInit.sol";
 import { Contact } from "./models/Contact.sol";
 import { ID } from "./models/ID.sol";
-import { PublicInfoCard } from "./models/PublicInfoCard.sol";
 import { PrivateInfoCard } from "./models/PrivateInfoCard.sol";
+import { PublicInfoCard } from "./models/PublicInfoCard.sol";
 
+/**
+ * @title Business Card Contract
+ * @dev This contract manages the creation and sharing of business cards and company profiles.
+ * It allows companies to create business cards for employees and users to share their cards.
+ */
 contract BusinnesCard is ERC721, Ownable {
 
+    /// @notice Emitted when a new business card is created.
+    /// @param owner The address that owns the card.
+    /// @param cardID The unique ID of the created card.
+    /// @param name The name associated with the card.
     event CardCreated(address indexed owner, uint256 cardID, string name);
+
+    /// @notice Emitted when a new company is created.
+    /// @param companyAddress The address of the company creator.
+    /// @param companyID The unique ID of the created company.
     event CompanyCreated(address indexed companyAddress, uint16 companyID);
+
+    /// @notice Emitted when a business card is shared with another user.
+    /// @param fromCard_ The address that shares their card.
+    /// @param to_ The address that receives the shared card.
     event SharedCard(address fromCard_, address indexed to_);
 
     address constant ZERO_ADDRESS = address(0);
 
+    /**
+     * @notice Constructor for the Business Card contract.
+     * @dev Initializes the ERC721 contract with the token name "Business Card" and symbol "BCARD".
+     */
     constructor() ERC721("Business Card", "BCARD") Ownable(msg.sender) { }
 
     //////////// Modifiers ///////////////
 
+    /**
+     * @dev Ensures that only registered companies can call certain functions.
+     */
     modifier onlyCompanies() {
         require(companiesID[msg.sender].exists, "Only registered companies");
         _;
     }
+
+    /**
+     * @dev Ensures that the provided address does not already have a business card.
+     * @param addr_ The address to check.
+     */
     modifier addressNotHaveCard(address addr_) {
         require(!cards[addr_].exists, "Address already has Card");
         _;
@@ -36,43 +65,73 @@ contract BusinnesCard is ERC721, Ownable {
     uint16 lastCompanyID;
     uint256 lastCardID;
     uint256 feeCreateCompany;
-   
+
     mapping(address => Card) cards;
     mapping(address => ID) companiesID;
-    mapping(uint16 => Company) companies; //La clave es el campo id del struct ID relacionada a la address del owner de la Company en el mapping companiesID
-    mapping(address => mapping(address => bool)) contacts; //
+    mapping(uint16 => Company) companies; // The key is the ID field from the ID struct related to the owner's address in companiesID
+    mapping(address => mapping(address => bool)) contacts; // Tracks if a card was shared with another address
 
     /////// Getters ////////////////////
+
+    /**
+     * @notice Get the ID of the company associated with the sender.
+     * @dev Only callable by registered companies.
+     * @return The ID of the sender's company.
+     */
     function getMyCompanyID() public view onlyCompanies returns(uint16) {
         return companiesID[msg.sender].id;
     }
 
+    /**
+     * @notice Get the name of a company by its ID.
+     * @param id_ The ID of the company.
+     * @return The name of the company.
+     */
     function getCompanyName(uint16 id_) public view returns(string memory) {
         return companies[id_].initValues.companyName;
     }
 
+    /**
+     * @notice Get the number of employees in a company by its ID.
+     * @param id_ The ID of the company.
+     * @return The number of employees in the company.
+     */
     function getEmployedQty(uint16 id_) public view returns(uint16) {
         return companies[id_].companyEmployees;
     }
-    
+
+    /**
+     * @notice Get the ID of the card owned by the sender.
+     * @return The ID of the sender's card.
+     */
     function getMyCardId() public view returns(uint256){
         return cards[msg.sender].publicInfo.cardID;
     }
+
     ////////////////////////////////////////////////////
 
+    /**
+     * @notice Set the fee required to create a new company profile.
+     * @dev Only callable by the contract owner.
+     * @param _fee The new fee amount in wei.
+     */
     function setFeeCreateCompany(uint256 _fee) public onlyOwner {
         feeCreateCompany = _fee;
     }
 
-    // Esta funcion crea un perfil de empresa que queda vinculado uno a uno a la address del sender
+    /**
+     * @notice Create a new company profile linked to the sender's address.
+     * @dev Requires a payment equal to or greater than the fee. Any excess is refunded.
+     * @param initValues_ The initial data for the company.
+     */
     function createCompany(CompanyInit memory initValues_) public payable {
         require(!companiesID[msg.sender].exists, "Company already exists");
         require(msg.value >= feeCreateCompany, "Insufficient payment");
-        /// Devolución de excedente (revisar)
+        /// Refund excess payment if any
         if (msg.value > feeCreateCompany) {
             payable(msg.sender).transfer(msg.value - feeCreateCompany);
         }
-        /////  Ver que hacer con los fondos ///////////////
+        ///// Process funds (decide what to do with the collected funds)
         Company memory newCompany = Company({
             initValues: initValues_,
             companyEmployees: 0,
@@ -83,25 +142,45 @@ contract BusinnesCard is ERC721, Ownable {
         companiesID[msg.sender] = ID({id: lastCompanyID, exists: true});
         companies[lastCompanyID] = newCompany;
         emit CompanyCreated(msg.sender, lastCompanyID);
-        
     }
 
-    function createCardFor( CardDataInit memory initValues_, address _for ) public onlyCompanies addressNotHaveCard(_for) {
+    /**
+     * @notice Create a new business card for an address.
+     * @dev Only callable by registered companies. The address must not already have a card.
+     * @param initValues_ The initial data for the business card.
+     * @param _for The address for which the card is being created.
+     */
+    function createCardFor(CardDataInit memory initValues_, address _for) public onlyCompanies addressNotHaveCard(_for) {
         initValues_.companyID = companiesID[msg.sender].id;
         _safeCreateCard(initValues_, _for);
-        companies[initValues_.companyID].companyEmployees ++;
+        companies[initValues_.companyID].companyEmployees++;
     }
-    
-    function createMyCard( CardDataInit memory initValues_ ) public addressNotHaveCard(msg.sender) {
+
+    /**
+     * @notice Create a new business card for the sender.
+     * @dev The sender must not already have a card.
+     * @param initValues_ The initial data for the business card.
+     */
+    function createMyCard(CardDataInit memory initValues_) public addressNotHaveCard(msg.sender) {
         _safeCreateCard(initValues_, msg.sender);
     }
 
+    /**
+     * @notice Share the sender's business card with another address.
+     * @dev The card must exist for the sender.
+     * @param to_ The address with whom the card is being shared.
+     */
     function shareMyCard(address to_) public {
         assert(cards[msg.sender].exists);
         contacts[msg.sender][to_] = true;
         emit SharedCard(msg.sender, to_);
     }
 
+    /**
+     * @dev Internal function to safely create a business card for a given address.
+     * @param initValues_ The initial data for the business card.
+     * @param to The address for which the card is being created.
+     */
     function _safeCreateCard(CardDataInit memory initValues_, address to) private {
         lastCardID++;
         Card memory newCard;
@@ -115,7 +194,5 @@ contract BusinnesCard is ERC721, Ownable {
         cards[to] = newCard;
         emit CardCreated(to, newCard.publicInfo.cardID, newCard.publicInfo.name);
     }
-
-
 
 }
